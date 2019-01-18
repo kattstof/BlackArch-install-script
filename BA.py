099a1a846addf784036de63e7af3fa4c5667006a
kattstof <katrinastofferman@autistici.org>
2019-01-18 12:56:32 -0600

    added refrences to montypython for pythonicness

--- BA.py
+++ BA.py
@@ -10,8 +10,8 @@ print (" ")
 print ("[1] Install All Blackarch *This will take a while go drink some coffee*")
 print("[2] Install certain Blackarch Tool")
 print ("[0] Exit")
-chce = input(":")
-if chce == '1':
+spam = input(":")
+if spam == '1':
         print("-------------------------------------")
         print("INSTALLING AND EXECUTING STRAP.SH     ")
         print("PLEASE WAIT.........                   ")
@@ -21,17 +21,22 @@ if chce == '1':
         subprocess.call("sudo chmod +x strap.sh", shell=True)
         subprocess.call("sudo ./strap.sh", shell=True)
         subprocess.call("sudo pacman -S blackarch", shell=True)
-if chce== '2':
+if spam == '2':
         print("------------------------------------")
         print("INSTALLING AND EXECUTING STRAP.SH    ")
         print("PLEASE WAIT.......                  ")
         print("------------------------------------")
+
         time.sleep(3)
         subprocess.call("curl -O https://blackarch.org/strap.sh", shell=True)
         subprocess.call("sudo chmod +x strap.sh", shell=True)
         subprocess.call("sudo ./strap.sh", shell=True)
-        choice = input("Which tool would you like to install?:")
-        subprocess.call("sudo pacman -S "  + choice, shell=True)
-if chce == '0':
-        print("Okay Scipt kiddie, Go back to HackForums")
+        eggs = input("Which tool would you like to install?:")
+        subprocess.call("sudo pacman -S "  + eggs, shell=True)
+if spam == '0':
+        print("If you do not go get us a shrubbery we shall nee you again")
+        time.sleep(1)
+        print("...one that looks good")
+        time.sleep(1)
+        print("...but not too expensive")
         exit()
